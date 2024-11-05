3e4fee5aea4a4fde3dba2b3692c350f4205cc81c
mingjiayu <91915656+mingjiayu@users.noreply.github.com>
2024-11-04 17:57:06 -0800

    Add files via upload

---
+++ Around/around-backend/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://35.247.70.100:9200"
+    ES_USERNAME = "123456"
+    ES_PASSWORD = "123456"
+    GCS_BUCKET = "chen-bucket"
+)
---
+++ Around/around-frontdnd/src/constants.js
@@ -0,0 +1,8 @@
+export const TOKEN_KEY = "token";
+export const BASE_URL = "http://35.247.70.100:8080";
+// export const BASE_URL = "https://symmetric-span-374017.uw.r.appspot.com"; //deployed on GAE
+export const SEARCH_KEY = {
+  all: 0,
+  keyword: 1,
+  user: 2,
+};
